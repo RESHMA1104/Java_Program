97d94e749e30df91219fefbb06352ab6f74299ea
RESHMA1104 <163125056+RESHMA1104@users.noreply.github.com>
2025-03-26 22:29:49 +0530

    Add files via upload

---
+++ Java Programs/Basics.java
@@ -0,0 +1,15 @@
+public class Basics {
+    public static void main(String[] args) {
+        char g = 'k'; // For character, single quotes only used
+        //String g = "k"; // For String, we can use double quotes
+         /*int a = 10;
+        int b = 20;
+        int c = a+b;
+        System.out.println(c);
+        */
+
+        //float d = 2.6f;
+        //double a = 20.8;
+        System.out.println(g);
+    }
+}
